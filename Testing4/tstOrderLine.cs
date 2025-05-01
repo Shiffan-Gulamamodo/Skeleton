a352258d5929721f596b92efd4b9980635f7c48d
p2781996 <lunasampaio25@icloud.com>
2025-05-01 12:29:11 +0100

    OrderLine Testing

---
+++ Testing4/tstOrderLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrderLine
+    {
+        [TestMethod]
+        public void InstanceOK1()
+        {
+            // create an instance of the class we want to create
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            // test that it exists
+            Assert.IsNotNull(AnOrderLine);
+        }
+    }
+}
