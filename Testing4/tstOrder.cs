026c551b1560b495baa9e547bf2abf93b34ffeda
p2781996 <lunasampaio25@icloud.com>
2025-05-06 10:28:05 +0100

    Added property testing for Order class

--- Testing4/tstOrder.cs
+++ Testing4/tstOrder.cs
@@ -13,5 +13,73 @@ namespace Testing4
             clsOrder AnOrder = new clsOrder();
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void OrderIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.OrderID = TestData;
+            Assert.AreEqual(AnOrder.OrderID, TestData);
+        }
+
+        [TestMethod]
+        public void StaffIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.StaffID = TestData;
+            Assert.AreEqual(AnOrder.StaffID, TestData);
+        }
+
+        [TestMethod]
+        public void OrderDatePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnOrder.OrderDate = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.OrderDate, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.CustomerID = TestData;
+            Assert.AreEqual(AnOrder.CustomerID, TestData);
+        }
+
+        [TestMethod]
+        public void IsPaidPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.IsPaid = TestData;
+            Assert.AreEqual(AnOrder.IsPaid, TestData);
+        }
+
+        [TestMethod]
+        public void DeliveryAddressPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string TestData = "123 Main St";
+            AnOrder.DeliveryAddress = TestData;
+            Assert.AreEqual(AnOrder.DeliveryAddress, TestData);
+        }
+
+        [TestMethod]
+        public void OrderLineIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.OrderLineID = TestData;
+            Assert.AreEqual(AnOrder.OrderLineID, TestData);
+        }
+
     }
 }
