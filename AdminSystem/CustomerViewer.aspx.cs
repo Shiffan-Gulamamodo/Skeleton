6ae3cd6fba047bff0219f2efbf5b5e9ba402e161
Shiffan-Gulamamodo <P2786133@my365.dmu.ac.uk>
2025-05-07 12:44:14 +0100

    Created a simple Basic Presesntation Layer for the Customer Component by adding Labels, TextBox, and Modifying some Properties. Also updated the Customer Data Entry and Customer Viewer File to get the data from the customer and pass it to the viewer file for it to show the data.

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //Create a new Instance of clsCustomer
+        clsCustomer ACustomer = new clsCustomer();
+        //Get the Data form the session Object 
+        ACustomer = (clsCustomer)Session["ACustomer"];
+        //Display the Customer Full Name for this entry
+        Response.Write(ACustomer.CustomerId + "<br />");
+        Response.Write(ACustomer.CustomerFullName + "<br />");
+        Response.Write(ACustomer.CustomerEmail + "<br />");
+        Response.Write(ACustomer.CustomerPassword + "<br />");
+        Response.Write(ACustomer.CustomerAddress + "<br />");
+        Response.Write(ACustomer.CustomerAccountCreatedAt + "<br />");
+        Response.Write(ACustomer.IsCustomerAccountActive);
 
     }
 }
