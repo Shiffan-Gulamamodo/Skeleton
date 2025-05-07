d26c5f984d46d56d9db6970ba8ac9c8f530066e0
d1p4nshu-479 <dvasant1109@gmail.com>
2025-05-07 14:13:49 +0100

    Delete dbo.tblSupplier.data.sql

--- dbo.tblSupplier.data.sql
+++
@@ -1,8 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[tblSupplier] ON
-INSERT INTO [dbo].[tblSupplier] ([SupplyId], [PhoneName], [DeliveryDate], [IsAvailable]) VALUES (1, N'Iphone 16', N'2025-06-01 00:00:00', 1)
-INSERT INTO [dbo].[tblSupplier] ([SupplyId], [PhoneName], [DeliveryDate], [IsAvailable]) VALUES (2, N'Iphone 17', N'2025-09-03 00:00:00', 1)
-INSERT INTO [dbo].[tblSupplier] ([SupplyId], [PhoneName], [DeliveryDate], [IsAvailable]) VALUES (3, N'Iphone 15', N'2025-06-06 00:00:00', 1)
-INSERT INTO [dbo].[tblSupplier] ([SupplyId], [PhoneName], [DeliveryDate], [IsAvailable]) VALUES (4, N'Iphone 14', N'2025-05-25 00:00:00', 0)
-INSERT INTO [dbo].[tblSupplier] ([SupplyId], [PhoneName], [DeliveryDate], [IsAvailable]) VALUES (5, N' Iphone 13', N'2025-03-02 00:00:00', 1)
-INSERT INTO [dbo].[tblSupplier] ([SupplyId], [PhoneName], [DeliveryDate], [IsAvailable]) VALUES (6, NULL, NULL, 0)
-SET IDENTITY_INSERT [dbo].[tblSupplier] OFF
