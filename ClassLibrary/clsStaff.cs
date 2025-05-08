93c04fcc22368dcb1725f36223ea73ee4bcb6791
d1p4nshu-479 <dvasant1109@gmail.com>
2025-05-08 20:12:12 +0100

    Added Find Method for the Staff Component for every attribute property.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,12 +4,117 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public bool isActive { get; set; }
-        public DateTime lastUpdated { get; set; }
-        public int StaffId { get; set; }
-        public string StaffFullName { get; set; }
-        public string StaffPassword { get; set; }
-        public string Email { get; set; }
-        public string Role { get; set; }
+        //private data member for the Staff ID property
+        private Int32 mStaffId;
+        private DateTime mLastUpdated;
+        private String mStaffFullName;
+        private String mStaffPassword;
+        private String mEmail;
+        private Boolean mIsActive;
+        private String mRole;
+
+        //Staff ID public property
+        public int StaffId {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffId;
+            } set
+            {
+                //this line of code allows data into the property
+                mStaffId = value;
+            } 
+        }
+        
+        public DateTime lastUpdated
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mLastUpdated;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mLastUpdated = value;
+            }
+        }
+        public bool isActive {
+            get
+            {
+                //this line of code sends data out of the property
+                return mIsActive;
+            }
+            set 
+            {
+                //this line of code allows data into the property
+                mIsActive = value;
+            } 
+        }
+        
+        public string StaffFullName {
+            get 
+            {
+                //this line of code sends data out of the property
+                return mStaffFullName;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffFullName = value;
+            }
+        }
+        public string StaffPassword 
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffPassword;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffPassword = value;
+            }
+        }
+        public string Email 
+        {
+            get
+            { 
+                //this line of code sends data out of the property
+                return mEmail;
+            }
+            set
+            { 
+                //this line of code allows data into the property
+                mEmail = value;
+            } 
+        }
+        public string Role 
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mRole;
+            }
+            set
+            { 
+                //this line of code allows data into the property
+                mRole = value;
+            } }
+
+        public bool Find(int staffId)
+        {
+            //set the private data members to the test data value
+            mStaffId = 3;
+            mStaffFullName = "Max Payne";
+            mStaffPassword = "brazilRio778";
+            mEmail = "MPayne@hotmail.com";
+            mLastUpdated = Convert.ToDateTime("09/08/2024");
+            mIsActive = true;
+            mRole = "Order Manager";
+            //always return true
+            return true;
+        }
     }
 }
