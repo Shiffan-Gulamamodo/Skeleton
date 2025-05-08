fb8ae4b351fcb3e9b5e20557b34681169b49fee2
Shiffan-Gulamamodo <P2786133@my365.dmu.ac.uk>
2025-05-08 16:06:25 +0100

    Adding Data Layer Find Method to Customer Component. Now the Customer Form has a find method if you type a customer id and click find it fetches the cuustomer data for all property from the database.

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -38,12 +38,37 @@ public partial class _1_DataEntry : System.Web.UI.Page
         //Capture the Customer Address
         ACustomer.CustomerAddress = txtCustomerAddress.Text;
         //Capture the Customer Account Created At
-        ACustomer.CustomerAccountCreatedAt = Convert.ToDateTime(txtCustomerAccountCreatedAt.Text);
+        ACustomer.CustomerAccountCreatedAt = Convert.ToDateTime(DateTime.Now);
         //Capture Active Check Box 
         ACustomer.IsCustomerAccountActive= chkIsCustomerAccountActive.Checked;
         //Stores the Customer Full Name in the session Object 
         Session["ACustomer"] = ACustomer;
         //Navigate to the View Page
         Response.Redirect("CustomerViewer.aspx");
+    }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //Create a new instance of clsCustomer 
+        clsCustomer ACustomer = new clsCustomer();
+        //Create a variable to store the primary key 
+        Int32 CustomerId;
+        //Create a variable to store the result of the find operation 
+        Boolean Found = false;
+        //Get the primary key entered by the user 
+        CustomerId = Convert.ToInt32(txtCustomerId.Text);
+        //Find the record 
+        Found = ACustomer.Find(CustomerId);
+        //If Found 
+        if (Found == true) 
+        {
+            //Displaying the values of the properties in the form 
+            txtCustomerFullName.Text = ACustomer.CustomerFullName;
+            txtCustomerEmail.Text = ACustomer.CustomerEmail.ToString();
+            txtCustomerPassword.Text = ACustomer.CustomerPassword;
+            txtCustomerAddress.Text = ACustomer.CustomerAddress.ToString();
+            txtCustomerAccountCreatedAt.Text = ACustomer.CustomerAccountCreatedAt.ToString();
+            chkIsCustomerAccountActive.Checked = ACustomer.IsCustomerAccountActive;
+        }
     }
 }
