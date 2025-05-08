a54a2a72ac4683f39abec186a2a212f1059fe7d9
JJ2245 <P2710129@my365.dmu.ac.uk>
2025-05-08 13:27:13 +0100

    middle layer

--- AdminSystem/SupplierDataEntry.aspx.cs
+++ AdminSystem/SupplierDataEntry.aspx.cs
@@ -4,11 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        clsSupplier ASupplier = new clsSupplier();
+        ASupplier.SupplyId = Convert.ToInt32(txtSupplierID.Text);
+        ASupplier.StockID = Convert.ToInt32(txtStockID.Text);
+        ASupplier.PhoneName = txtPhoneName.Text;
+        ASupplier.DeliveryDate = Convert.ToDateTime(txtDeliveryDate.Text);
+        ASupplier.IsAvailable = chkIsAvailable.Checked;
+        Session["ASupplier"] = ASupplier;
+        Response.Redirect("SupplierViewer.aspx");
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtDeliveryDate_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void chkIsAvailable_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("");
     }
 }
