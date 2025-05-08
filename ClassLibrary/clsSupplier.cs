c2eae237959fad10e077cb4eb3eaf397ec9d1051
JJ2245 <p2710129@my365.dmu.ac.uk>
2025-05-08 12:45:58 +0100

    Update clsSupplier.cs

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        public decimal Price { get; set; }
+        public Boolean IsAvailable { get; set; }
+        public int StockID { get; set; }
+        public int SupplyId { get; set; }
+        public string PhoneName { get; set; }
+        public DateTime DeliveryDate { get; set; }
     }
 }
