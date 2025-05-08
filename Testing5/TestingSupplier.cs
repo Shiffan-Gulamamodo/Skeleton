3f777e40f36e8ee60cf1d8ac68d9fdd12e7caf0f
JJ2245 <P2710129@my365.dmu.ac.uk>
2025-05-08 12:28:20 +0100

    New Supplier

---
+++ Testing5/TestingSupplier.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing5
+{
+    [TestClass]
+    public class TestingSupplier
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier ASupplier = new clsSupplier();
+            //test to see that it exists
+            Assert.IsNotNull(ASupplier);
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AStock = new clsStock();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStock.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void StockIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            int TestData = 1;
+            //assign the data to the property
+            AStock.StockID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.StockID, TestData);
+        }
+
+        [TestMethod]
+        public void SupplierIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            int TestData = 1;
+            //assign the data to the property
+            AStock.SupplierID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.SupplierID, TestData);
+        }
+
+        [TestMethod]
+        public void ProductNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            string TestData = "iPhone 14 Pro Max";
+            //assign the data to the property
+            AStock.ProductName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.ProductName, TestData);
+        }
+
+        [TestMethod]
+        public void StockQuantityPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            int TestData = 10;
+            //assign the data to the property
+            AStock.StockQuantity = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.StockQuantity, TestData);
+        }
+
+        [TestMethod]
+        public void InStockPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            bool TestData = true;
+            //assign the data to the property
+            AStock.InStock = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.InStock, TestData);
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            decimal TestData = 9999.99m;
+            //assign the data to the property
+            AStock.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.Price, TestData);
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
